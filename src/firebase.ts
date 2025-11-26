2c9c0e61f37b21dbaeea6b320ee251bded1d7092
Woohyuk Kim <dngurvv@google.com>
2025-11-26 16:58:48 +0900

    feat: configure Firebase with environment variables and enable guestbook functionality in CommentForm

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,7 +4,8 @@ import { getDatabase } from 'firebase/database';
 // TODO: https://firebase.google.com/docs/web/setup?hl=ko
 // 가이드에 따라 firebase config를 설정합니다.
 const firebaseConfig = {
-
+    databaseURL: import.meta.env.VITE_APP_FIREBASE_DATABASE_URL,
+    projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
 };
 
 export const firebaseApp = initializeApp(firebaseConfig);
