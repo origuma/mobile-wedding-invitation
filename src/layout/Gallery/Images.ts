6db76996adac098b6c8eb938643960e0d1001bf4
Woohyuk Kim <dngurvv@google.com>
2025-12-30 22:07:26 +0900

    ?몄슜臾??ㅽ????섏젙: ?뺥쁽二?<?ㅼ떆, ?щ옉> 以묒뿉???띿뒪???고듃 ?ш린 異뺤냼 諛??ㅻⅨ履??뺣젹, 以꾧컙寃?議곗젙

--- src/layout/Gallery/Images.ts
+++ src/layout/Gallery/Images.ts
@@ -4,7 +4,7 @@ import image03 from '@/assets/images/03.jpg'
 import image04 from '@/assets/images/04.jpg'
 import image05 from '@/assets/images/05.jpg'
 import image06 from '@/assets/images/06.jpg'
-import image07 from '@/assets/images/07.jpg'
+//import image07 from '@/assets/images/07.jpg'
 import image08 from '@/assets/images/08.jpg'
 import image09 from '@/assets/images/09.jpg'
 import image10 from '@/assets/images/10.jpg'
@@ -46,21 +46,21 @@ const images = [   /* 이미지 경로를 리스트로 저장 */
     source: image06,
     width: 640,
     height: 960
-  },{
-    alt: 'image07',
-    source: image07,
-    width: 640,
-    height: 960
+  //},{
+    //alt: 'image07',
+    //source: image07,
+    //width: 640,
+    //height: 960
   },{
     alt: 'image08',
     source: image08,
     width: 640,
     height: 960
-  },{
-    alt: 'image09',
-    source: image09,
-    width: 640,
-    height: 960
+  //},{
+  //  alt: 'image09',
+  //  source: image09,
+  //  width: 640,
+  //  height: 960
   },{
     alt: 'image10',
     source: image10,
